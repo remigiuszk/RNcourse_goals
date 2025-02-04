e379b8839f6c27088bc80bb4cecb2eb6a5e1a5e1
rkaminski <remigiusz20kaminski@gmail.com>
2025-02-04 22:43:09 +0100

    handling the deleting

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItem = (props) => {
   return (
-    <View style={styles.goalContainer}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    <Pressable key={props.id} onPress={props.onDeleteItem.bind(this, props.id)}>
+      <View style={styles.goalContainer}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 };
 
