c8b9f76e9713036eb7a33fd4ee0cc5593cde71c8
dzem0r123 <dzem0r123@gmail.com>
2025-02-04 22:43:09 +0100

    handling the deleting

--- App.js
+++ App.js
@@ -6,12 +6,18 @@ import GoalItem from "./components/GoalItem";
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function addGoalHandler(enteredGoalText) {
+  const addGoalHandler = (enteredGoalText) => {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      { text: enteredGoalText, key: Math.random().toString() },
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
-  }
+  };
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  };
 
   return (
     <View style={styles.appContainer}>
@@ -20,7 +26,16 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
           }}
         />
       </View>
